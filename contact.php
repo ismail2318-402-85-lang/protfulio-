94b69f503bfce93431b0358a7da07a619778cf88
ismail2318-402-85-lang <ismail2318-402-85@bsdi-bd.org>
2026-07-20 23:48:00 +0600

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,25 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", "", "portfolio");
+
+if (!$conn) {
+    die("Connection Failed: " . mysqli_connect_error());
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$sql = "INSERT INTO contacts(name,email,subject,message)
+        VALUES('$name','$email','$subject','$message')";
+
+if(mysqli_query($conn,$sql)){
+    echo "Message Sent Successfully!";
+}else{
+    echo "Error: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+?>
